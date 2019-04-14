667a75ac97dcc18a89971710f9e7d529009a5047
eyeKill <1229522864@qq.com>
2019-04-14 11:30:13 +0800

    Add pi camera support

--- aruco_detect.py
+++ aruco_detect.py
@@ -2,22 +2,22 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 
-cap = cv2.VideoCapture(0)
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+        corners, ids, rejecttedImgPoints = aruco.detectMarkers(frame, aruco_dict)
+        frame = aruco.drawDetectedMarkers(frame, corners, ids)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    corners, ids, rejecttedImgPoints = aruco.detectMarkers(frame, aruco_dict)
-    frame = aruco.drawDetectedMarkers(frame, corners, ids)
-
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ pi_camera.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# import the necessary packages
+from imutils.video import FPS
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import imutils
+import time
+import cv2
+import cv2.aruco as aruco
+ 
+# initialize the camera and stream
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+stream = camera.capture_continuous(rawCapture, format="bgr",
+          use_video_port=True)
+
+print("sampling will start in 1 seconds...")
+time.sleep(1.0)
+fps = FPS().start()
+print("sampling started.")
+
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+
+for i, f in enumerate(stream):
+    frame = f.array
+    # frame = imutils.resize(frame, width=400)
+
+    corners, ids, rejecttedImgPoints = aruco.detectMarkers(frame, aruco_dict)
+    frame = aruco.drawDetectedMarkers(frame, corners, ids)
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if chr(key) == 'q':
+        break
+    
+    rawCapture.truncate(0)
+    fps.update()
+
+fps.stop()
+print("elasped time: " + str(fps.elapsed()))
+print("approx. FPS: " + str(fps.fps()))
+
+cv2.destroyAllWindows()
+stream.close()
+rawCapture.close()
+camera.close()
+
