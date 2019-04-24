bec26c7beb4b62ae9779512be7ed76b1068d53be
eyeKill <1229522864@qq.com>
2019-04-24 15:21:06 +0800

    Calibration started

---
+++ camera_calibration.py
@@ -0,0 +1,61 @@
+"""
+Camera calibration
+Remove camera distortion for further 3D pose estimation and positioning.
+References:
+https://www.cnblogs.com/wildbloom/p/8320351.html
+https://blog.csdn.net/firemicrocosm/article/details/48594897
+"""
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# chessboard size
+cb_size = (9,6)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((cb_size[0]*cb_size[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:cb_size[0], 0:cb_size[1]].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('cali/*.jpg')
+
+# sampling
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, cb_size, None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, cb_size, corners, ret)
+        cv2.imshow('img',img)
+        cv2.waitkey(300)
+
+cv2.destroyAllWindows()
+
+# calibration
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+print(ret)
+print(mtx)
+print(dist)
+# undistort
+h, w = img.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png',dst)
