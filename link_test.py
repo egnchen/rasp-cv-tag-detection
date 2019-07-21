7f12cd1c0ba4a6f6be0612e837a7e9a1e377cc70
eyeKill <1229522864@qq.com>
2019-07-21 22:04:12 +0800

    add link test scripts

---
+++ link_test.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+
+
+
+import time
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+
+from pymavlink import mavutil
+
+
+
+
+vehicle = connect('/dev/ttyS0',baud = 921600,wait_ready = True)
+
+
+
+def arm_and_takeoff(aTargetAltitude):
+    
+	    
+	print("Basic pre-arm checks")
+    
+	
+	while not vehicle.is_armable:
+        
+		print(" Waiting for vehicle to initialise...")
+        
+		time.sleep(1)
+
+    
+
+	
+	print("Arming motors")
+    
+	
+
+	vehicle.mode = VehicleMode("GUIDED")
+  
+	    
+	vehicle.armed = True
+
+    
+
+	   
+	while not vehicle.armed:
+        
+		print(" Waiting for arming...")
+        
+		time.sleep(1)
+
+    
+
+	
+    
+	print("Taking off!")
+    
+	
+    
+	vehicle.simple_takeoff(aTargetAltitude)
+
+    
+
+	
+    
+	while True:
+        
+		print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+  
+		
+
+		if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+ 
+			print("Reached target altitude")
+
+			break
+
+			time.sleep(1)
+
+
+'''def send_nav_velocity(self,v_x,v_y,v_z):
+	
+
+	msg =self.vehicle.message_factory.set_position_target_local_ned_encode(
+		0,
+		0,
+		0,
+		mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+		0b0000111111000111,
+		0, 0, 0,
+		v_x, v_y, v_z,
+		0, 0, 0,
+		0, 0)
+		
+		self.vihecle.send_mavlink(msg)
+		self.vehicle.flush()
+'''
+
+
+arm_and_takeoff(2)
+time.sleep(10)
+
+
+'''print("Set default/target airspeed to 3")
+
+
+vehicle.airspeed = 3
+
+
+
+point1 = LocationGlobalRelative(31.026546,121.422065,2)
+
+# point2 = LocationGlobalRelative()
+
+# point3 = LocationGlobalRelative()
+
+# point4 = LocationGlobalRelative()
+
+# point5 = LocationGlobalRelative()
+
+
+vehicle.simple_goto(point1)
+
+
+time.sleep(5)
+
+
+if ids is not None:
+	vehicle.wait_for_alt(1)
+	
+	while abs(tvec[0]+0.3)>0.2||abs(tvec[2]-0.8)>0.2:
+		if tvec[0]+0.3>0.2:
+			send_nav_velocity(,0,1,0)
+			time.sleep(abs(tvec[0]+0.3))
+		elif tvec[0]+0.3<-0.2:
+			send_nav_velocity(,0,-1,0)			
+			time.sleep(abs(tvec[0]+0.3))
+		if tvec[2]-0.8>0.2:
+			send_nav_velocity(,0,0,-1)
+			time.sleep(abs(tvec[2]-0.8))
+		elif tvec[2]-0.8<-0.2:
+			send_nav_velocity(,0,0,1)
+			time.sleep(abs(tvec[2]-0.8))
+
+send_nav_velocity(,-1,0,0)
+time.sleep(15)
+
+
+
+#vehicle.mode = VehicleMode("RTL")
+'''
+
+
+vehicle.armed = False
+
+
+print("Close vehicle object")
+
+vehicle.close()
---
+++ test.py
@@ -0,0 +1,28 @@
+from dronekit import connect
+
+vehicle = connect('/dev/ttyS0',baud = 921600,wait_ready = True)
+
+print("Autopilot Firmware version: %s" % vehicle.version)
+print("Global Location: %s" % vehicle.location.global_frame)
+print("Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+print("Local Location: %s" % vehicle.location.local_frame)
+print("Attitude: %s" % vehicle.attitude)
+print("Velocity: %s" % vehicle.velocity)
+print("GPS: %s" % vehicle.gps_0)
+print("Groundspeed: %s" % vehicle.groundspeed)
+print("Airspeed: %s" % vehicle.airspeed)
+print("Gimbal status: %s" % vehicle.gimbal)
+print("Battery: %s" % vehicle.battery)
+print("EKF OK?: %s" % vehicle.ekf_ok)
+print("Rangefinder: %s" % vehicle.rangefinder)
+print("Heading: %s" % vehicle.heading)
+print("Is Armable?: %s" % vehicle.is_armable)
+print("System status: %s" % vehicle.system_status.state)
+
+
+
+print("Mode: %s" % vehicle.mode.name)
+
+
+
+print("Armed: %s" % vehicle.armed)
