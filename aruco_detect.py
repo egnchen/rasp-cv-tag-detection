a629810f1310859f5df72bf9eb85082f1b9b8b69
eyeKill <1229522864@qq.com>
2019-10-07 16:52:11 +0800

    implemented & tested pic stream

--- aruco_detect.py
+++ aruco_detect.py
@@ -7,42 +7,38 @@ import math
 
 # local modules
 import params
-from camera_helper import USBVideoStream, FPS
+from camera_helper import USBVideoStream, PicVideoStream, FPS
 
 if __name__ == "__main__":
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-    fps_counter = FPS()
-    stream = USBVideoStream().start()
-    fps_counter.start()
+    stream = PicVideoStream("/home/hongji-li/cam_plane", "default_typhoon_h480_cgo3_camera_link_camera(1)-", sync=True).start()
 
-    while(fps_counter.frames() < 5000):
+    while(stream.fps_counter.frames() < 5000):
         # Capture frame-by-frame
         ret, frame = stream.read()
-        fps_counter.update()
-        print(fps_counter.currentFps())
+        print("fps", stream.get_fps())
 
         corners, ids, rejecttedImgPoints = aruco.detectMarkers(frame, aruco_dict)
         frame = aruco.drawDetectedMarkers(frame, corners, ids)
 
         if ids is not None:    
-            rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, 0.15, params.mtx, params.dist)
-            prompt_strings = []
-            for rvec, tvec in zip(rvecs, tvecs):
-                print("rvec ", rvec)
-                print("tvec ", tvec)
-                frame = aruco.drawAxis(frame, params.mtx, params.dist, rvec, tvec, 0.15)
-                prompt_strings.append("x:{:.2f} y:{:.2f} z:{:.2f}".format(*(tvec[0])))
+            ret = aruco.estimatePoseSingleMarkers(corners, 0.15, params.mtx, params.dist)
+            print(ret)
+            #prompt_strings = []
+            #for rvec, tvec in zip(rvecs, tvecs):
+               # print("rvec ", rvec)
+              #  print("tvec ", tvec)
+               # frame = aruco.drawAxis(frame, params.mtx, params.dist, rvec, tvec, 0.15)
+                # prompt_strings.append("x:{:.2f} y:{:.2f} z:{:.2f}".format(*(tvec[0])))
                 # prompt_strings.append("x:{:.2f} y:{:.2f} z:{:.2f}".format(*(rvec[0])))
-            cv2.putText(frame, '\n'.join(prompt_strings), (25,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+           # cv2.putText(frame, '\n'.join(prompt_strings), (25,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
         # Display the resulting frame
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        fps_counter.wait(40)
 
     # When everything done, stop the stream
     stream.stop()
-    print("time elapsed(s)", fps_counter.elapsed())
-    print("average fps", fps_counter.fps())
+    print("average fps", stream.get_fps())
     cv2.destroyAllWindows()
--- camera_helper.py
+++ camera_helper.py
@@ -4,15 +4,18 @@ Camera module helper functions.
 """
 
 import cv2
+import os
 import time
-from threading import Thread
+import glob
+from threading import Thread, Lock
+from queue import Queue
 
 class FPS:
     def __init__(self):
         self._start = None
         self._end = None
         self._lastUpdate = None
-        self._lastElasped = None
+        self._lastElapsed = 0 
         self._numFrames = 0
 
     def start(self):
@@ -42,10 +45,69 @@ class FPS:
         return self._numFrames / self.elapsed()
 
     def wait(self, millis):
-        time.sleep((millis - self._lastElapsed)*0.001)
+        time.sleep((millis - self._lastElapsed) * 0.001)
+
+
+class PicVideoStream:
+
+    def __init__(self, path, prefix, filetype="jpg", sync=False, framerate=40):
+        self.path = path
+        self.prefix = prefix
+        self.filetype = filetype
+        self._sync = sync
+        self._stopped = False
+        self._update_flag = False
+        self.fps_counter = FPS()
+        self._buffer = Queue() 
+        if self._sync:
+            self.wait_time = 1000 / framerate
+        
+    def start(self, offset=None):
+        if offset == None:
+            # get all filenames that meets the standard
+            l = glob.glob(os.path.join(self.path, "".join((self.prefix, "*.", self.filetype))))
+            # convert to numbers
+            l = [int(x[len(os.path.join(self.path, self.prefix)): -1 - len(self.filetype)]) for x in l]
+            # get the latest value
+            self.num = max(l)
+        else:
+            self.num = offset
+        print(f"Starting from {self.num}")
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def get_filename(self, offset = 0):
+        return os.path.join(self.path, "".join((self.prefix, str(self.num + offset), '.', self.filetype)))
+
+    def update(self):
+        self.fps_counter.start()
+        while True:
+            if self._sync:
+                self.fps_counter.wait(self.wait_time)
+            self.fps_counter.update()
+            
+            # place offset=1 here
+            # to avoid incomplete picture
+            fn = self.get_filename(1)
+            while not os.path.exists(fn):
+                time.sleep(0.001)
+
+            self._buffer.put((self.num, cv2.imread(self.get_filename())))
+            self.num += 1
+    
+    def read(self):
+        return self._buffer.get()
+
+    def stop(self):
+        self._stopped = True
+        self.fps_counter.stop()
+
+    def get_fps(self):
+        return self.fps_counter.currentFps()
+
 
 class USBVideoStream:
-    def __init__(self, src=0):
+    def __init__(self, src=0, sync=False, framerate=30):
         # initialize the video camera stream and read the first frame
         self.stream = cv2.VideoCapture(src)
         self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -55,15 +117,27 @@ class USBVideoStream:
         # initialize the indicate value
         self.stopped = False
         self._update_flag = False
+        self._update_lock = Lock()
+        self._sync = sync
+        self.fps_counter = FPS()
+        if self._sync:
+            # synchronized update
+            self.wait_time = 1 / framerate
 
     def start(self):
         Thread(target=self.update, args=()).start()
         return self
 
     def update(self):
-        while True:
+        self.fps_counter.start()
+
+        while not self._stopped:
             if self.stopped:
                 return
+            if self._sync:
+                self.fps_counter.wait(self.wait_time)
+            self.fps_counter.update()
+
             # grab frame from stream
             self.grabbed, self.frame = self.stream.read()
             self._update_flag = True
@@ -72,10 +146,15 @@ class USBVideoStream:
         while not self._update_flag:
             pass
         
+        ret = self.grabbed, self.frame
         self._update_flag = False
-        return self.grabbed, self.frame
+        return ret
+
     
     def stop(self):
         self.stopped = True
+        self.fps_counter.stop()
         self.stream.release()
 
+    def get_fps(self):
+        return self.fps_counter.currentFps()
